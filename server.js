9bdfae12127ee6e2c274441030f2997719256936
manik prakash <manikprakash74@gmail.com>
2026-02-11 23:55:12 +0530

    updated server

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.delete("/todos/:id", (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log("Server running on port " + PORT);
 });
